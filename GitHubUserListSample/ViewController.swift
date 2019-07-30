5cef395aa9cee0efcd78e180acd459b107492a18
Tomohiro Imaizumi <bonriki.life@gmail.com>
2019-07-30 12:51:48 +0900

    Initial Commit

---
+++ GitHubUserListSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubUserListSample
+//
+//  Created by 今泉 智博 on 2019/07/30.
+//  Copyright © 2019 imaizume. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GitHubUserListSampleTests/GitHubUserListSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  GitHubUserListSampleTests.swift
+//  GitHubUserListSampleTests
+//
+//  Created by 今泉 智博 on 2019/07/30.
+//  Copyright © 2019 imaizume. All rights reserved.
+//
+
+import XCTest
+@testable import GitHubUserListSample
+
+class GitHubUserListSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
